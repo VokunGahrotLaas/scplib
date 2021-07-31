b1457ce58f2e39fb3a13fc462e6f0141651a610c
Maxence MICHOT <maxence.michot.pro@gmail.com>
2021-07-31 21:17:03 +0200

    shortened code with macro

--- scp/exceptions.h
+++ scp/exceptions.h
@@ -6,22 +6,30 @@
 #include <stdarg.h>
 #include <stdnoreturn.h>
 
+#define SCP_MAKE_EXCEPTION(name, code, func, funcf)								\
+noreturn void func(const char* message) {										\
+	fprintf(stderr, "scpException %s: '%s'\n", name, message);					\
+	exit(code);																	\
+}																				\
+																				\
+noreturn void funcf(const char* format, ...) {									\
+	fprintf(stderr, "scpException %s: '", name);								\
+	va_list args;																\
+	va_start(args, format);														\
+	vfprintf(stderr, format, args);												\
+	va_end(args);																\
+	fputs("'\n", stderr);														\
+	exit(code);																	\
+}
+
 typedef enum scpEsceptionCode {
 	scpExceptionCode_Success = EXIT_SUCCESS,
 	scpExceptionCode_Failure = EXIT_FAILURE,
 	scpExceptionCode_OutOfBound,
 	scpExceptionCode_NullPointer,
-	scpExceptionCode_NotImplemented
+	scpExceptionCode_NotImplemented,
+	scpExceptionCode_InvalidArgument
 } scpExitCode;
-
-noreturn void scpException_Exception(int status, const char* message);
-noreturn void scpException_Exceptionf(int status, const char* format, ...);
-noreturn void scpException_OutOfBound(const char* message);
-noreturn void scpException_OutOfBoundf(const char* format, ...);
-noreturn void scpException_NullPointer(const char* message);
-noreturn void scpException_NullPointerf(const char* format, ...);
-noreturn void scpException_NotImplemented(const char* message);
-noreturn void scpException_NotImplementedf(const char* format, ...);
 
 noreturn void scpException_Exception(int status, const char* message) {
 	fprintf(stderr, "scpException (%i): '%s'\n", status, message);
@@ -38,49 +46,9 @@ noreturn void scpException_Exceptionf(int status, const char* format, ...) {
 	exit(status);
 }
 
-noreturn void scpException_OutOfBound(const char* message) {
-	fprintf(stderr, "scpException OutOfBound: '%s'\n", message);
-	exit(scpExceptionCode_OutOfBound);
-}
-
-noreturn void scpException_OutOfBoundf(const char* format, ...) {
-	fputs("scpException OutOfBound: '", stderr);
-	va_list args;
-	va_start(args, format);
-	vfprintf(stderr, format, args);
-	va_end(args);
-	fputs("'\n", stderr);
-	exit(scpExceptionCode_OutOfBound);
-}
-
-noreturn void scpException_NullPointer(const char* message) {
-	fprintf(stderr, "scpException NullPointer: '%s'\n", message);
-	exit(scpExceptionCode_NullPointer);
-}
-
-noreturn void scpException_NullPointerf(const char* format, ...) {
-	fputs("scpException NullPointer: '", stderr);
-	va_list args;
-	va_start(args, format);
-	vfprintf(stderr, format, args);
-	va_end(args);
-	fputs("'\n", stderr);
-	exit(scpExceptionCode_NullPointer);
-}
-
-noreturn void scpException_NotImplemented(const char* message) {
-	fprintf(stderr, "scpException NotImplemented: '%s'\n", message);
-	exit(scpExceptionCode_NotImplemented);
-}
-
-noreturn void scpException_NotImplementedf(const char* format, ...) {
-	fputs("scpException NotImplemented: '", stderr);
-	va_list args;
-	va_start(args, format);
-	vfprintf(stderr, format, args);
-	va_end(args);
-	fputs("'\n", stderr);
-	exit(scpExceptionCode_NotImplemented);
-}
+SCP_MAKE_EXCEPTION("OutOfBound", scpExceptionCode_OutOfBound, scpException_OutOfBound, scpException_OutOfBoundf)
+SCP_MAKE_EXCEPTION("NullPointer", scpExceptionCode_NullPointer, scpException_NullPointer, scpException_NullPointerf)
+SCP_MAKE_EXCEPTION("NotImplemented", scpExceptionCode_NotImplemented, scpException_NotImplemented, scpException_NotImplementedf)
+SCP_MAKE_EXCEPTION("InvalidArgument", scpExceptionCode_InvalidArgument, scpException_InvalidArgument, scpException_InvalidArgumentf)
 
 #endif // SCP_EXCEPTIONS_H
