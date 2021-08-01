a5f4e1508b280b252f76f97463e33187719d9f1f
Maxence MICHOT <maxence.michot.pro@gmail.com>
2021-08-01 19:40:28 +0200

    fixed macros for string concatenation

--- scp/macros.h
+++ scp/macros.h
@@ -1,7 +1,12 @@
 #ifndef SCP_MACROS_H
 #define SCP_MACROS_H
 
-#define SCP_STRINGIZER(x) #x
-#define SCP_TO_STRING(x) SCP_STRINGIZER(x)
+#define SCP_TO_STRING_NX(x) #x
+#define SCP_TO_STRING(x) SCP_TO_STRING_NX(x)
+
+#define SCP_CONCAT_NX(a, b) a ## b
+#define SCP_CONCAT(a, b) SCP_CONCAT_NX(a, b)
+
+#define SCP_LAMBDA(return_type, body) ({ return_type _ body _; })
 
 #endif // SCP_MACROS_H
